aefb177f08339a98457f4958b852394433d4b011
Asadbek <ahikmatullayev024@gmail.com>
2023-12-11 06:56:59 +0500

    no changes;

--- profile_2/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import profile_2_view
-
-
-urlpatterns = [
-    path('', profile_2_view)
-]
--- profile_2/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def profile_2_view(request):
-    return render(request, 'profile_2.html')
