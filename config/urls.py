40c50c0d00a62a073d694135e896bfbef7218e6b
bonuruzikulova <bonu.ruzikulova@mail.ru>
2023-12-14 01:14:33 +0500

    fully ready results

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("results.urls")),
-    path("", include("about_us.urls"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- results/urls.py
+++ results/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import results_view
 
 urlpatterns = [
-    path("results/", results_view, name="results" )
+    path("results/", results_view, name="results")
 ]
