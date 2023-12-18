c12d72c0b25c8a73d60da866ed888e6f6580cee3
Asadbek <ahikmatullayev024@gmail.com>
2023-12-18 11:02:49 +0500

    finished without media

--- participant_profile/views.py
+++ participant_profile/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 
 
 def profile_view(request):
-    return render(request, 'profile.html')
+    return render(
+        request,
+        'profile.html',
+        context={
+            'competitions': [
+                {
+                    'img': {'url': 'static/profile/img/competition.png', },
+                    'title': 'Испытай себя',
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'
+                },
+                {
+                    'img': {'url': 'static/profile/img/competition.png', },
+                    'title': 'Испытай себя',
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'
+                },
+                {
+                    'img': {'url': 'static/profile/img/competition.png', },
+                    'title': 'Испытай себя',
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'
+                },
+                {
+                    'img': {'url': 'static/profile/img/competition.png', },
+                    'title': 'Испытай себя',
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut'
+                }
+            ]
+        }
+    )
