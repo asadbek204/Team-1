d3242fce30f4cabb67a3c7b38ecf7436f2548a36
Asadbek <ahikmatullayev024@gmail.com>
2023-12-19 18:05:10 +0500

    added regions templates

---
+++ assets/profile/js/profile.js
@@ -0,0 +1,3 @@
+const colors = ['#F5C24D', '#F6510A', '#0000FF', '#F5CF4B', '#F5F24D', '#F651AA', '#000FFF', '#F5B24C']
+for (let id of array) document.getElementById(id).style.backgroundColor = colors[Math.round(Math.random()*10+1)]
+
--- regions/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'regions'
