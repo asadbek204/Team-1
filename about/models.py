2f2759a3369e882acc51875063f8632576731456
Abdullokh <nosirovabdulloh952@gmail.com>
2023-12-21 18:32:20 +0500

    models are creating

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+
+class Result(models.Model):
+    #expert
+    score = models.PositiveIntegerField()
--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class AdminModel(models.Model):
+    # user
+    rules = models.CharField()
+
+
+class RuleModel(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    admin_site = models.ForeignKey(AdminModel, on_delete=models.CASCADE)
