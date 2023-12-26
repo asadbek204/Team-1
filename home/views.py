ea980da205ce38d9ab006201848bea188125c321
Abdullokh <nosirovabdulloh952@gmail.com>
2023-12-26 23:12:20 +0500

    gallery view done

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import GalleryModel
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home/main_page.html')
 
 
-def gallery(request):
-    return render(request, 'home/gallery.html')
+class GalleryListView(ListView):
+    model = GalleryModel
+    template_name = 'home/gallery.html'
+    context_object_name = 'obj'
+    paginate_by = 9
+
+    def get_queryset(self):
+        return GalleryModel.objects.all()
