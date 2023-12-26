ae5cf88d25a47ab7414d8edffb5dc1ebb618de3b
Asadbek <ahikmatullayev024@gmail.com>
2023-12-26 22:06:14 +0500

    small fix

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('competition.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
