ebb1b3fb15739491f6cfc28ef5424fb445cb1838
Asadbek <ahikmatullayev024@gmail.com>
2023-12-27 00:28:23 +0500

    started profile

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views.login import *
+from .views.profile import profile
+
 urlpatterns = [
-    path('', login, name='login')
+    path('', login, name='login'),
+    path('profile/', profile, name='profile')
 ]
