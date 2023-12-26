1f135dee7cff97b96ca2740a3955bc09bfc9d892
bonuruzikulova <bonu.ruzikulova@mail.ru>
2023-12-26 23:55:15 +0500

    first commit

--- about/models.py
+++ about/models.py
@@ -11,3 +11,18 @@ class Result(models.Model):
     score = models.PositiveIntegerField()
     expert = models.ForeignKey('account.Expert', on_delete=models.CASCADE)
     questionnaire = models.ForeignKey('competition.QuestionnaireModel', on_delete=models.CASCADE)
+
+
+class CounterModel(models.Model):
+    criteria = models.CharField(max_length=100, unique=True)
+    img = models.ImageField(upload_to="criteria/")
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.criteria
+
+    class Meta:
+        verbose_name = "counter"
+        verbose_name_plural = "counters"
+        db_table = "counter"
