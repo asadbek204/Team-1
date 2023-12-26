87a3e03a8749de763da3f160773b9d4da9230be0
Abdullokh <nosirovabdulloh952@gmail.com>
2023-12-26 23:10:09 +0500

    home views

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class GalleryModel(models.Model):
+    img = models.ImageField(upload_to='home/most_images/')
+    title = models.CharField(max_length=100)
