2796c85f4923099df24f279fca81dac63bed6358
Asadbek <ahikmatullayev024@gmail.com>
2023-12-29 00:30:50 +0500

    connected account.urls

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
     path('questionare/', include('competition.urls'))
 ]
 
