1af09457f083a70870130d10918bf13023a90e12
Asadbek <ahikmatullayev024@gmail.com>
2023-12-30 22:23:40 +0500

    fixing expert:urls.py

--- expert/urls.py
+++ expert/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import expert_registration
 
 urlpatterns = [
-    path("", expert_registration.ExpertRegView.as_view(), name="expert_reg")
-
+    path("login/", expert_registration.ExpertRegView.as_view(), name="expert_reg")
 ]
