0dadb27092270c5101df73cbd9a93323fe1c8470
Asadbek <ahikmatullayev024@gmail.com>
2023-12-31 15:28:09 +0500

    connecting search to home:urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import views
+from .views import views, search
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('gallery/', views.GalleryListView.as_view(), name='gallery')
+    path('gallery/', views.GalleryListView.as_view(), name='gallery'),
+    path('search/', search.SearchView.as_view(), name='search')
 ]
