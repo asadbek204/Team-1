ed890752515f9433db954ae082fef956f8c0e48e
Asadbek <ahikmatullayev024@gmail.com>
2024-01-02 09:33:50 +0500

    fixed settings and urls

--- assets/base/js/base.js
+++ assets/base/js/base.js
@@ -1,42 +1,34 @@
 const searchWindow = document.getElementById('search-window')
-const generalContainer = document.getElementById('general-container')
-const viewport = document.getElementById('viewport')
-
 const searchInput = document.getElementById('search-input')
 const searchLabel = document.getElementById('search-label')
-
-const searchUrl = baseUrl + '/search/'
-const csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value
-const btnCloseWindow = document.getElementById('btn-close-window')
 const dateInput = document.getElementById('date-input')
 const byDescriptionCheck = document.getElementById('by-description')
 const btnSubmit = document.getElementById('btn-submit')
-
-byDescriptionCheck.onchange = (event) => {
-    searchLabel.innerText = (event.target.checked) ? 'search by description' : 'search by title'
-}
-
-btnSubmit.onclick = (event) => event.preventDefault()
-
-const requests = new Requests(searchUrl)
-
+const btnCloseWindow = document.getElementById('btn-close-window')
+const generalContainer = document.getElementById('general-container')
+const viewport = document.getElementById('viewport')
+const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value
+const requests = new Requests(baseUrl + '/search/', csrfToken)
 function blurPage() {
     generalContainer.classList.toggle('blur')
-    document.body.classList.toggle('on-search')
+    document.body.classList.toggle('freeze')
 }
 function openSearchWindow() {
     searchWindow.classList.toggle('active')
     btnCloseWindow.classList.toggle('display-none')
 }
-
-async function renderCompetitions(competitions){
-    for (let competition of competitions) {
+async function renderCompetitions(data){
+    if (data.competitions.length === 0) {
+        viewport.innerHTML = `<span class="not-found">${data.message}</span>`
+        return
+    }
+    for (let competition of data.competitions) {
         let a = document.createElement('a')
         a.href = `${baseUrl}/competition/detail/${competition.id}`
         a.className = "competition"
         a.innerHTML = `
             <div class="img-block">
-                <img src="${baseUrl}/${competition.photo.url}" alt="">
+                <img class="competition-img" src="${baseUrl}/${competition.photo.url}" alt="">
             </div>
             <div class="info-block">
                 <span class="title">${competition.title}</span>
@@ -46,50 +38,31 @@ async function renderCompetitions(competitions){
         viewport.appendChild(a)
     }
 }
-
-const getData = async () => {
-    const result = await requests.GET()
-    const data = await result.json()
-    if (data.competitions.length === 0) {
-        viewport.innerHTML = `<span class="not-found">${data.message}</span>`
-        return
-    }
-    await renderCompetitions(data.competitions)
+const format = (data) => `${(data < 10) ? '0': ''}${(data)}`
+const formatDate = (d) => d.getFullYear()+"-"+format(d.getMonth()+1)+"-"+format(d.getDate())
+const search = async () => {
+    const value = searchInput.value.toLowerCase()
+    let body = {start_date: (dateInput.value.length > 0) ? dateInput.value : formatDate(new Date(Date.now()))}
+    if (byDescriptionCheck.checked) body.description = value
+    else body.title = value
+    await renderCompetitions(await (await requests.POST(body)).json())
 }
-
-function formattedDate(d)
-{
-    const format = (data) => `${(data < 10) ? '0': ''}${(data)}`
-    return d.getFullYear()+"-"+format(d.getMonth()+1)+"-"+format(d.getDate())
-}
-
-searchInput.oninput = async (event) => {
-    const value = event.target.value
-    const date = new Date(Date.now())
-    let body = {start_date: (dateInput.value.length > 0) ? dateInput.value : formattedDate(date)}
-    if (byDescriptionCheck.checked) {
-        body.description = value
-    }
-    else {
-        body.title = value
-    }
-    const result = await requests.POST(body)
-    const data = await result.json()
-    if (data.competitions.length === 0) {
-        viewport.innerHTML = `<span class="not-found">${data.message}</span>`
-        return
-    }
-    await renderCompetitions(data.competitions)
-}
-
+searchInput.oninput = search
+dateInput.onchange = search
 btnCloseWindow.onclick = () => {
     openSearchWindow()
     blurPage()
 }
-
-document.getElementById('search-button').onclick = () => {
+document.getElementById('search-button').onclick = async () => {
     blurPage()
     openSearchWindow()
     searchInput.focus()
-    getData().then()
+    if (viewport.children.length <= 1) await renderCompetitions(await (await requests.GET()).json())
+}
+byDescriptionCheck.onchange = async (event) => {
+    searchLabel.innerText = (event.target.checked) ? 'search by description' : 'search by title'
+    await search()
+}
+btnSubmit.onclick = (event) => {
+    event.preventDefault()
 }
