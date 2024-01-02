8c169d578cdda012bd8e073316f46d5c9a6f9102
Asadbek <ahikmatullayev024@gmail.com>
2024-01-02 14:41:59 +0500

    realized change_password

--- assets/expert/expert_registaration/main/js/main.js
+++ assets/expert/expert_registaration/main/js/main.js
@@ -1,11 +1,12 @@
 const usernameInput = document.getElementById('username')
 const passwordInput = document.getElementById('password')
+const message = document.getElementById("message2")
 requests.url = url
 document.getElementById("regBtn").onclick = async (event) => {
     event.preventDefault()
     let body = {username: usernameInput.value, password: passwordInput.value}
     const data = await (await requests.POST(body)).json()
-    // if (!data.ok) return document.getElementById("message1").innerText = data.message
+    if (!data.ok) return document.getElementById("message1").innerText = data.message
     blurPage()
     const popup = document.getElementById('popup')
     popup.classList.toggle('active')
@@ -14,6 +15,9 @@ document.getElementById('regBtn2').onclick = async (event) => {
     event.preventDefault()
     const newUsernameInput = document.getElementById('newpassword')
     const newPasswordInput = document.getElementById('newpassword')
-    if ((usernameInput.value === newUsernameInput.value) || (passwordInput.value === newPasswordInput.value)) return document.getElementById("message2").innerText = "you must change your username and password"
-
+    if ((usernameInput.value === newUsernameInput.value) || (passwordInput.value === newPasswordInput.value)) return message.innerText = "you must change your username and password"
+    let body = {username: usernameInput.value, password: passwordInput.value, newusername: newUsernameInput.value, newpassword: newPasswordInput.value}
+    const data = await (await requests.PUT(body)).json()
+    if (!data.ok) return message.innerText = data.message
+    document.location.reload(true)
 }
--- expert/views/expert_registration.py
+++ expert/views/expert_registration.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth import login
 from account.models import Expert, UserModel
 import json
 from django.views.generic import View
@@ -9,13 +10,15 @@ class ExpertRegView(View):
 
     @staticmethod
     def get(request):
+        if request.user.is_authenticated:
+            return redirect('matching')
         return render(request, template_name="expert/expert_registration.html")
 
     @staticmethod
     def post(request):
         data = json.loads(request.body)
         try:
-            user = UserModel.objects.get(**data)
+            user = UserModel.objects.get(username=data['username'], password=data['password'])
         except UserModel.DoesNotExist:
             result = {'ok': False, 'message': 'user does not exist'}
         else:
@@ -24,10 +27,18 @@ class ExpertRegView(View):
             except Expert.DoesNotExist:
                 result = {'ok': False, 'message': 'expert does not exist'}
             else:
-                result = {'ok': True, 'message': 'success', 'expert': expert}
+                result = {'ok': True, 'message': 'success', 'expert_id': expert.id}
         return JsonResponse(result)
 
-    @staticmethod
-    def put(request):
+    def put(self, request):
+        data = self.post(request)
+        if data.get('ok', False):
+            return JsonResponse({'ok': False, 'message': data.get('message')})
+        expert = Expert.objects.get(id=data.get('expert_id'))
         data = json.loads(request.body)
-        return JsonResponse({'ok': True, 'message': ''})
+        if data.get('newusername', False) and data.get('newpassword', False):
+            expert.user.username = data['newusername']
+            expert.user.set_password(data['newpassword'])
+            expert.user.save()
+            login(request, expert.user)
+        return JsonResponse({'ok': True, 'message': 'success'})
