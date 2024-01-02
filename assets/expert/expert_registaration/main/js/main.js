74495d8cd041f7608ae0166a186c2c41302638cd
Asadbek <ahikmatullayev024@gmail.com>
2024-01-02 11:09:58 +0500

    changed base.html:class Requests

--- assets/expert/expert_registaration/main/js/main.js
+++ assets/expert/expert_registaration/main/js/main.js
@@ -1,12 +1,19 @@
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
 requests.url = url
 document.getElementById("regBtn").onclick = async (event) => {
     event.preventDefault()
-    const usernameInput = document.getElementById('username')
-    const usernamePassword = document.getElementById('password')
-    let body = {username: usernameInput.value, password: usernamePassword.value}
+    let body = {username: usernameInput.value, password: passwordInput.value}
     const data = await (await requests.POST(body)).json()
     // if (!data.ok) return document.getElementById("message1").innerText = data.message
     blurPage()
     const popup = document.getElementById('popup')
     popup.classList.toggle('active')
+}
+document.getElementById('regBtn2').onclick = async (event) => {
+    event.preventDefault()
+    const newUsernameInput = document.getElementById('newpassword')
+    const newPasswordInput = document.getElementById('newpassword')
+    if ((usernameInput.value === newUsernameInput.value) || (passwordInput.value === newPasswordInput.value)) return document.getElementById("message2").innerText = "you must change your username and password"
+
 }
