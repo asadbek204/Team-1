020c5e4f2f9cb77ea29beba65e50ef6ffd260a91
abu0717 <nosirovabdulloh952@gmail.com>
2024-01-03 23:04:25 +0500

    something

--- account/views/registration.py
+++ account/views/registration.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, redirect
+from account.forms import SignUpForm
 
 
 def register_view(request):
-    return render(request, 'account/register.html')
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            del form.cleaned_data['confirm_password']
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('home')
+    return render(request, 'account/register.html', context={
+        "form": form
+    })
