3d18c465dcf0f29d45b89901d0714438d440143b
Asadbek <ahikmatullayev024@gmail.com>
2024-01-05 22:47:43 +0500

    started frogot password

---
+++ account/views/forgot_password.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from account.models import UserModel
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.generic import View
+
+
+class ForgotPasswordView(View):
+
+    @staticmethod
+    @login_required('login')
+    def get(request):
+        return render(request)
+
+    @staticmethod
+    @login_required('login')
+    def post(request):
+        return JsonResponse({'ok': True, 'message': 'success'})
