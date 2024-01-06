5c4a37a5e443771d41045776ce034170349722b3
Asadbek <ahikmatullayev024@gmail.com>
2024-01-07 02:43:15 +0500

    finished backend but frontend so ugly

--- account/views/forgot_password.py
+++ account/views/forgot_password.py
@@ -1,7 +1,5 @@
-import json
-import random
-import string
-import time
+import json, random, string, time
+from django.contrib.auth import login
 from account.models import UserModel
 from django.http import JsonResponse
 from django.views.generic import View
@@ -41,21 +39,32 @@ class ForgotPasswordView(View):
             return JsonResponse({'ok': False, 'message': 'user not found'})
         password = self.get_new_password(UserModel.objects.get(username=data.get('username')))
         request.session['password'] = password
+        request.session['time'] = time.time()
         return JsonResponse({'ok': True, 'message': 'please check your email'})
 
     @staticmethod
     def put(request):
         data = json.loads(request.body)
+        if request.user.is_authenticated:
+            if not data.get('password', False):
+                return JsonResponse({'ok': False, 'message': 'password is required'})
+            request.user.set_password(request.session.pop('password'))
+            request.user.save()
+            return JsonResponse({'ok': True, 'message': 'successfully updated'})
         if not (data.get('username', False) and data.get('email', False)):
             return JsonResponse({'ok': False, 'message': 'username and email please'})
         if not UserModel.objects.filter(username=data.get('username'), email=data.get('email')).exists():
             return JsonResponse({'ok': False, 'message': 'user not found'})
         if not data.get('password', False):
             return JsonResponse({'ok': False, 'message': 'enter your password please'})
+        if not request.session.get('password', False):
+            return JsonResponse({'ok': False, 'message': 'you are not forgotten to change'})
         if data['password'] != request.session['password']:
             return JsonResponse({'ok': False, 'message': 'wrong password'})
-        if time.time() - request.session['time'] < 60:
+        if time.time() - request.session['time'] > 60:
             return JsonResponse({'ok': False, 'message': 'time limit exceeded'})
-        user = request.user if request.user.is_authenticated else UserModel.objects.get(username=data.get('username'))
-        user.set_password(request.session.pop('password', None))
-        return JsonResponse({'ok': True})
+        user = UserModel.objects.get(username=data.get('username'))
+        user.set_password(request.session.pop('password'))
+        user.save()
+        login(request, user)
+        return JsonResponse({'ok': True, 'message': 'successfully updated'})
